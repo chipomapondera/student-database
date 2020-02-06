c1a8a034ab320fb78f66ed577a623624ff00ebc5
chipo mapondera <chipo.mapondera@ynap.com>
2020-02-06 16:34:06 +0000

    student database with frontend ui pulling data from database

---
+++ src/main/java/com/example/sbfullstack/SbFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbfullstack/SbFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
